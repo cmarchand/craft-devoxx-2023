21d64bf15e2710b87ea11088a9b33c754ccf899d
Christophe Marchand <christophe@marchand.top>
2023-04-11 10:52:07 +0200

    FizzBuzz terminé

--- src/main/java/com/oxiane/katas/fizzbuzz/FizzBuzz.java
+++ src/main/java/com/oxiane/katas/fizzbuzz/FizzBuzz.java
@@ -2,6 +2,21 @@ package com.oxiane.katas.fizzbuzz;
 
 public class FizzBuzz {
   public static String fizzBuzz(int input) {
+    if(isFizzBuzz(input)) return "FizzBuzz";
+    if(isFizz(input)) return "Fizz";
+    if(isBuzz(input)) return "Buzz";
     return Integer.toString(input);
+  }
+
+  private static boolean isFizzBuzz(int input) {
+    return isFizz(input) && isBuzz(input);
+  }
+
+  private static boolean isBuzz(int input) {
+    return input % 5 == 0;
+  }
+
+  private static boolean isFizz(int input) {
+    return input % 3 == 0;
   }
 }
--- src/test/java/com/oxiane/katas/fizzbuzz/FizzBuzzTest.java
+++ src/test/java/com/oxiane/katas/fizzbuzz/FizzBuzzTest.java
@@ -18,11 +18,77 @@ public class FizzBuzzTest {
     Assertions.assertThat(actual).isEqualTo(expected);
   }
   @Test
-  @DisplayName("Given number is one, FizzBuzz should return \"1\"")
+  @DisplayName("Given number is two, FizzBuzz should return \"2\"")
   public void given_2_should_return_2() {
     // Given
     int input = 2;
     String expected = "2";
+    // When
+    String actual = FizzBuzz.fizzBuzz(input);
+    // Then
+    Assertions.assertThat(actual).isEqualTo(expected);
+  }
+  @Test
+  @DisplayName("Given number is three, FizzBuzz should return \"Fizz\"")
+  public void given_3_should_return_Fizz() {
+    // Given
+    int input = 3;
+    String expected = "Fizz";
+    // When
+    String actual = FizzBuzz.fizzBuzz(input);
+    // Then
+    Assertions.assertThat(actual).isEqualTo(expected);
+  }
+  @Test
+  @DisplayName("Given number is five, FizzBuzz should return \"Buzz\"")
+  public void given_5_should_return_Buzz() {
+    // Given
+    int input = 5;
+    String expected = "Buzz";
+    // When
+    String actual = FizzBuzz.fizzBuzz(input);
+    // Then
+    Assertions.assertThat(actual).isEqualTo(expected);
+  }
+  @Test
+  @DisplayName("Given number is six, FizzBuzz should return \"Fizz\"")
+  public void given_6_should_return_Fizz() {
+    // Given
+    int input = 6;
+    String expected = "Fizz";
+    // When
+    String actual = FizzBuzz.fizzBuzz(input);
+    // Then
+    Assertions.assertThat(actual).isEqualTo(expected);
+  }
+  @Test
+  @DisplayName("Given number is ten, FizzBuzz should return \"Buzz\"")
+  public void given_10_should_return_Buzz() {
+    // Given
+    int input = 10;
+    String expected = "Buzz";
+    // When
+    String actual = FizzBuzz.fizzBuzz(input);
+    // Then
+    Assertions.assertThat(actual).isEqualTo(expected);
+  }
+  @Test
+  @DisplayName("Given number is fifteen, FizzBuzz should return \"FizzBuzz\"")
+  public void given_15_should_return_FizzBuzz() {
+    // Given
+    int input = 15;
+    String expected = "FizzBuzz";
+    // When
+    String actual = FizzBuzz.fizzBuzz(input);
+    // Then
+    Assertions.assertThat(actual).isEqualTo(expected);
+  }
+  @Test
+  @DisplayName("Given number is thirty, FizzBuzz should return \"FizzBuzz\"")
+  public void given_30_should_return_FizzBuzz() {
+    // Given
+    int input = 30;
+    String expected = "FizzBuzz";
     // When
     String actual = FizzBuzz.fizzBuzz(input);
     // Then

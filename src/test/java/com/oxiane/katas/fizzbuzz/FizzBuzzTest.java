dcba1ad2106a9a3c7996f013908645c390e23388
Christophe Marchand <christophe@marchand.top>
2023-04-11 10:17:42 +0200

    FizzBuzz: Création test 1

---
+++ src/test/java/com/oxiane/katas/fizzbuzz/FizzBuzzTest.java
@@ -0,0 +1,20 @@
+package com.oxiane.katas.fizzbuzz;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class FizzBuzzTest {
+
+  @Test
+  @DisplayName("Given number is one, FizzBuzz should return \"1\"")
+  public void given_1_should_return_1() {
+    // Given
+    int input = 1;
+    String expected = "1";
+    // When
+    String actual = FizzBuzz.fizzBuzz(input);
+    // Then
+    Assertions.assertThat(actual).isEqualTo(expected);
+  }
+}
